98efa4b05b870aa69670125e341e7ecbc9696915
oxqlion <rafinaya1127@gmail.com>
2023-10-17 13:04:35 +0700

    hiyguft

---
+++ app/src/main/java/com/example/movieapps/ui/view/ListMovieView.kt
@@ -0,0 +1,182 @@
+package com.example.movieapps.ui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.movieapps.model.Movie
+import com.example.movieapps.viewmodel.ListMovieUIState
+import com.example.movieapps.viewmodel.ListMovieViewModel
+
+
+@Composable
+fun ListMovieView(
+    movieList: List<Movie>,
+    onFavClicked: (Movie) -> Unit,
+    onCardClick: (Movie) -> Unit
+){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+    ){
+        items(movieList){movie ->
+
+            var isLikedView by rememberSaveable { mutableStateOf(movie.isLiked)}
+
+            MovieCard(
+                movie = movie,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxSize(),
+                onFavClicked = {
+                    onFavClicked(movie)
+                    isLikedView = movie.isLiked
+                },
+                isLikedView = isLikedView,
+                onCardClicked = { onCardClick(movie) }
+            )
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieCard(
+    movie: Movie,
+    modifier: Modifier = Modifier,
+    onFavClicked: () -> Unit,
+    onCardClicked: (Movie) -> Unit,
+    isLikedView: Boolean,
+){
+    Card(
+        modifier = modifier,
+        onClick = {onCardClicked(movie)}
+    ) {
+        Column {
+            Box(
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Image(
+                    painter = painterResource(id = movie.poster_path),
+                    contentDescription = "Movie image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp),
+                    contentScale = ContentScale.FillWidth
+                )
+
+                FloatingActionButton(
+                    onClick = onFavClicked,
+                    shape = CircleShape,
+                    modifier = Modifier.padding(end = 4.dp, bottom = 4.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = "Favorite",
+                        tint = if(isLikedView){
+                            Color(0xFFEC407A)
+                        }else{
+                            Color(0xFF636363)
+                        }
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                Text(
+                    text = movie.title,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .weight(2f)
+                        .height(60.dp)
+                )
+
+                Text(
+                    text = "(${movie.getYear()})",
+                    textAlign = TextAlign.Right,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+
+            Row(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(
+                    imageVector = Icons.Filled.Star,
+                    contentDescription = "Star",
+                    tint = Color(0xFFFDCC0D)
+                )
+                Text(
+                    text = "${movie.vote_average}/10.0",
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+
+            Text(
+                text = movie.overview,
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Left
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun ListMoviePreview(){
+    val listMovieViewModel: ListMovieViewModel = viewModel()
+    val status = listMovieViewModel.listMovieUIState
+    when(status){
+        is ListMovieUIState.Loading -> {}
+        is ListMovieUIState.Success -> ListMovieView(
+            movieList = status.data,
+            onFavClicked = {movie ->
+                listMovieViewModel.onFavClicked(movie)
+            },
+            {}
+        )
+        is ListMovieUIState.Error ->{}
+    }
+}
---
+++ app/src/main/java/com/example/movieapps/ui/view/ProfileView.kt
@@ -0,0 +1,290 @@
+package com.example.movieapps.ui.view
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.movieapps.R
+import kotlinx.coroutines.launch
+import java.util.regex.Pattern
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileView(){
+
+    var name by rememberSaveable { mutableStateOf("") }
+    var phone by rememberSaveable { mutableStateOf("") }
+    var age by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+
+    var isEmailValid by rememberSaveable { mutableStateOf(true) }
+    var isPasswordValid by rememberSaveable { mutableStateOf(true) }
+
+    val snackbarHostState = remember {SnackbarHostState()}
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = {SnackbarHost(snackbarHostState)},
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_profile),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, Color.Gray, CircleShape)
+                )
+                CustomTextField(
+                    value = name,
+                    onValueChanged = {name = it} ,
+                    text = "Name",
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+                CustomTextField(
+                    value = phone,
+                    onValueChanged = {phone = it} ,
+                    text = "Phone",
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Next
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+                CustomTextField(
+                    value = age,
+                    onValueChanged = {age = it} ,
+                    text = "Age",
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+                CustomEmailField(
+                    value = email,
+                    onValueChanged = {email = it} ,
+                    text = "Email",
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    isEmailValid = isEmailValid
+                )
+                CustomPasswordField(
+                    value = password,
+                    onValueChanged = {password = it} ,
+                    text = "Password",
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    isPasswordValid = isPasswordValid
+                )
+
+                Button(
+                    onClick = {
+                        isEmailValid = isValidEmail(email)
+                        isPasswordValid = isValidPassword(password)
+
+                        if (isEmailValid && isPasswordValid){
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    "Data $name saved"
+                                )
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 16.dp),
+                    enabled = name.isNotBlank()&&phone.isNotBlank()&&age.isNotBlank()&&email.isNotBlank()&&password.isNotBlank()
+                )
+                {
+                    Text(text = "Submit")
+                }
+            }
+        }
+    )
+
+
+}
+
+// Function to validate email using regex
+fun isValidEmail(email: String): Boolean {
+    val emailPattern = Pattern.compile(
+        "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$",
+        Pattern.CASE_INSENSITIVE
+    )
+    return emailPattern.matcher(email).matches()
+}
+
+// Function to validate password
+fun isValidPassword(password: String): Boolean {
+    val passwordPattern = Pattern.compile(
+        "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=!])(?=\\S+\$).{8,}\$"
+    )
+    return passwordPattern.matcher(password).matches()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    text: String,
+    keyboardOptions: KeyboardOptions,
+    modifier: Modifier = Modifier
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        label = { Text(text = text)},
+        keyboardOptions = keyboardOptions,
+        modifier = modifier
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomEmailField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    text: String,
+    keyboardOptions: KeyboardOptions,
+    modifier: Modifier = Modifier,
+    isEmailValid: Boolean
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        label = { Text(text = text)},
+        keyboardOptions = keyboardOptions,
+        modifier = modifier,
+        isError = !isEmailValid
+    )
+
+    if (!isEmailValid){
+        Text(
+            text = "Invalid email format",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 4.dp),
+            Color.Red
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomPasswordField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    text: String,
+    keyboardOptions: KeyboardOptions,
+    modifier: Modifier = Modifier,
+    isPasswordValid: Boolean
+){
+
+    var isPasswordVisible by remember { mutableStateOf(false)}
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        label = { Text(text = text)},
+        keyboardOptions = keyboardOptions,
+        modifier = modifier,
+        isError = !isPasswordValid,
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            IconButton(
+                onClick = { isPasswordVisible = !isPasswordVisible }
+            ) {
+                Icon(
+                    imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                    contentDescription = if (isPasswordVisible) "Hide password" else "Show password"
+                )
+            }
+        },
+    )
+
+    if (!isPasswordValid){
+        Text(
+            text = "Password must be 8 characters long and contain uppercase, lowercase, number, and special character",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 4.dp),
+            Color.Red
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun ProfilePreview(){
+    ProfileView()
+}
